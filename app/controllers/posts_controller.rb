56421f763e452bcc42420a204027017cb4871122
chinweokwu <paulcmorah@gmail.com>
2020-08-11 23:33:42 +0100

    added registration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    @posts = Post.new
   end
 
   # GET /posts/1
