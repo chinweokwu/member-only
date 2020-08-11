3b27167c2ec3bc5e550bf17cef36ebba1a6d0705
Laguna1 <lagunazoomarket@gmail.com>
2020-08-12 00:26:35 +0300

    add user_id to posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 30 }
-  validates :body, presence: true, length: { maximum: 500 }
-  belongs_to :user
+  validates :post,    presence: true, length: { maximum: 500 }
+ # belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_170612) do
+ActiveRecord::Schema.define(version: 2020_08_11_211820) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
