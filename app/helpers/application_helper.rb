acb4a3c9dc075e0c75a8ecf187d7cf3f03dc6a92
chinweokwu <paulcmorah@gmail.com>
2020-08-16 00:39:12 +0100

    added logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def login_helper(current_user)
+        if user_signed_in? 
+          (link_to current_user.name, edit_user_registration_path, class: "button is-primary") +
+          (link_to "Logout", destroy_user_session_path, method: :delete, class: "button is-primary ml-3" )
+        else 
+          (link_to 'Sign In', new_user_session_path, class: "control button is-primary" )   +  
+          (link_to 'Sign Up', new_user_registration_path, class: "control button is-primary ml-3")   
+        end 
+    end
 end
