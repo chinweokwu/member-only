68ffb5ee7a48157fb3694b36d786b9861b9ceed5
Laguna1 <lagunazoomarket@gmail.com>
2020-08-17 21:09:06 +0300

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
   def login_helper(current_user)
-      if user_signed_in? 
-        (link_to current_user.name, edit_user_registration_path, class: "button is-primary") +
-        (link_to "Logout", destroy_user_session_path, method: :delete, class: "button is-primary ml-3" )
-      else 
-        (link_to 'Sign In', new_user_session_path, class: "control button is-primary" )   +  
-        (link_to 'Sign Up', new_user_registration_path, class: "control button is-primary ml-3")   
-      end 
+    if user_signed_in?
+      (link_to current_user.name, edit_user_registration_path, class: 'button is-primary') +
+        (link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button is-primary ml-3')
+    else
+      (link_to 'Sign In', new_user_session_path, class: 'control button is-primary') +
+        (link_to 'Sign Up', new_user_registration_path, class: 'control button is-primary ml-3')
+    end
   end
 
-  def notification(notice, alert) 
-       if notice
-          content_tag(:p,  notice , :class => 'notice notification is-primary global-notification')     
-      elsif  alert 
-          content_tag(:p,  alert , :class => 'alert notification is-danger global-notification')
-      end
+  def notification(notice, alert)
+    if notice
+      content_tag(:p, notice, class: 'notice notification is-primary global-notification')
+    elsif alert
+      content_tag(:p, alert, class: 'alert notification is-danger global-notification')
+    end
   end
 end
